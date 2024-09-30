055921d1c4ddb620ffda868baf7cb589658d0ec8
farahelb <farah.elb26@gmail.com>
2024-09-30 11:27:04 +0300

    Part 2 week-fe-pp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,25 +7,29 @@ import Navbar from "./components/Navbar";
 import NotFoundPage from "./pages/NotFoundPage"
 import JobPage from './pages/JobPage';
 import EditJobPage from './pages/EditJobPage';
+import Signup from './pages/Signup';
+import Login from "./pages/Login";
 
 const App = () => {
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path='*' element={<NotFoundPage />} />
-              <Route path="/jobs/:id" element={<JobPage />} />
-              <Route path="/edit-job/:id" element={<EditJobPage />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-job" element={<AddJobPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+            <Route path="/jobs/:id" element={<JobPage />} />
+            <Route path="/edit-job/:id" element={<EditJobPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
